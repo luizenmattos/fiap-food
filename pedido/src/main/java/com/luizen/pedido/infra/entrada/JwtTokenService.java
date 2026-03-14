b1d9fcee3cfbf7a27fdabff2161ea12a816cc1cb
Luiz Mattos <lhamattos@outlook.com>
2026-03-14 11:25:33 -0300

    bug: erro 500 -> nao estava sendo lancado excecao tratada

--- pedido/src/main/java/com/luizen/pedido/infra/entrada/JwtTokenService.java
+++ pedido/src/main/java/com/luizen/pedido/infra/entrada/JwtTokenService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.luizen.pedido.aplicacao.entrada.ApplicationException;
 import com.luizen.pedido.aplicacao.entrada.token.TokenService;
 import com.luizen.pedido.aplicacao.entrada.token.UsuarioToken;
 
@@ -16,15 +17,20 @@ public class JwtTokenService implements TokenService {
     private String secret;
 
     public UsuarioToken obterDadosDoToken(String token) {
-        Algorithm algoritmo = Algorithm.HMAC256(secret);
-        DecodedJWT decodedJWT = JWT.require(algoritmo)
-                .withIssuer("Luizen Auth API")
-                .build()
-                .verify(token);
+        try{
+            Algorithm algoritmo = Algorithm.HMAC256(secret);
+            DecodedJWT decodedJWT = JWT.require(algoritmo)
+                    .withIssuer("Luizen Auth API")
+                    .build()
+                    .verify(token);
+    
+            UUID id = UUID.fromString(decodedJWT.getSubject());
+            String papel = decodedJWT.getClaim("papel").asString();
+            return new UsuarioToken(id, papel);
 
-        UUID id = UUID.fromString(decodedJWT.getSubject());
-        String papel = decodedJWT.getClaim("papel").asString();
-        return new UsuarioToken(id, papel);
+        }catch(Exception e){
+            throw new ApplicationException("Token inválido ou expirado");
+        }
 
     }
 
